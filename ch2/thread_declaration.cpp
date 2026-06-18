fb26bbeffb339f479024eafeb692cf67d928ee9c
Colin Le <colintranle123@gmail.com>
2026-06-17 17:14:45 -0700

    threads pass in rvalue args to the function call and if the function call is non const, it means that there is a possibility of modifiying the rvalue

--- ch2/thread_declaration.cpp
+++ ch2/thread_declaration.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <thread>
 
-class Hello{
+class Hello {
 public:
-  void operator()(){
-  std::cout << "Hello from Hello Class" << std::endl;
-  }
+  void operator()() { std::cout << "Hello from Hello Class" << std::endl; }
+};
+
+class X {
+public:
+  void do_work() { std::cout << "Inside Class X" << std::endl; }
 };
 
 // Need to add const to tell the complier
 // message will not be modified because message
 // is a short-lived copy
-void print_message(const std::string& message){
+void print_message(const std::string &message) {
   std::cout << message << std::endl;
 }
-int main(){
+int main() {
   Hello hello;
   std::thread t1(hello);
   t1.join();
-  
+
   // Thread creates a temporary short-lived copy
   std::thread t2(print_message, "Hello from t2");
   t2.join();
-
+  
   std::thread t3{print_message, "Hello from t3"};
   t3.join();
- 
-  std::thread t4([](){
-    std::cout << "Hello from t4" << std::endl;
-  });
+
+  std::thread t4([]() { std::cout << "Hello from t4" << std::endl; });
   t4.join();
+
+  X my_x;
+  std::thread t5(&X::do_work, &my_x);
+  t5.join();
   return 0;
 }
